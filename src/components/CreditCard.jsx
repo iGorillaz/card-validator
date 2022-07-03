c862b52fd7ba371dd926322be884f37bbf9b7ba1
iGorillaz <barannik.denis.92@gmail.com>
2022-07-03 17:58:22 +0300

    Initial project

---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,14 @@
+import { Button, Form, Alert, Row, Col } from "react-bootstrap";
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css'
+
+
+const CreditCard = () => {
+	return ( 
+		<div>
+
+		</div>
+	 );
+}
+ 
+export default CreditCard;
